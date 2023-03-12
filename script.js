9031cd1d48ccda4a04d662c11b9d34f11e30bf89
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-03-12 14:37:50 -0400

    Finished InClass Practice

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-// window.onload = () => {
-//     let rightAnswer = document.getElementsByClassName("right-answer")[0]
-//     let wrongAnswer = document.getElementsByClassName("wrong-answer")[0]
-//     rightAnswer.onclick = rightAnswerClick
-//     wrongAnswer.onclick = wrongAnswerClick
-// }
 
+// window.onload = () => {
+//     let rightAnswers = document.getElementsByClassName("right-answer")
+//     let wrongAnswers = document.getElementsByClassName("wrong-answer")
+//     // rightAnswer.onclick = rightAnswerClick
+//     // wrongAnswer.onclick = wrongAnswerClick
+
+//     for (const rightAnswer of rightAnswers) {
+//         rightAnswer.onclick = rightAnswerClick; 
+//     }
+    
+//     for (const wrongAnswer of wrongAnswers) {
+//         wrongAnswer.onclick = wrongAnswerClick;  
+//     }
+
+
+// }
 // let rightAnswerClick = (event) => {
 //     event.target.classList.add("green")
 // }
@@ -12,3 +22,35 @@
 // let wrongAnswerClick = (event) => {
 //     event.target.classList.add("red")
 // }
+
+
+
+window.onload = () => {
+    let questions = document.getElementsByClassName("question-box")
+
+    for (const question of questions) {
+        let rightAnswers = question.getElementsByClassName("right-answer")
+        let wrongAnswers = question.getElementsByClassName("wrong-answer")
+        console.log(wrongAnswers)
+        let answered = false;
+
+        for (const rightAnswer of rightAnswers) {
+            rightAnswer.onclick = (event) => {
+                if (!answered) {
+                    event.target.classList.add("green")
+                    answered = true;
+                }
+            };
+        }
+
+        for (const wrongAnswer of wrongAnswers) {
+            wrongAnswer.onclick = (event) => {
+                if (!answered) {
+                    event.target.classList.add("red")
+                    answered = true;
+                }
+            };
+        }
+    }
+};
+
